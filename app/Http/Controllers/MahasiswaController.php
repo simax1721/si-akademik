8d72bca970f4eab9231324f2a44a2e95b00fb0ce
syaukas <200705051@student.ar-raniry.ac.id>
2022-12-11 13:41:13 +0700

    setengah

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Datajurusan;
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function index()
+    {
+        $mahasiswas = Mahasiswa::all();
+        return view('mahasiswa.index', compact('mahasiswas'));
+    }
+
+    public function create()
+    {
+        $jurusans = Datajurusan::all();
+        return view('mahasiswa.create', compact('jurusans'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nim' => 'required|unique:mahasiswas|max:9',
+            'name' => 'required',
+            'tgl_lahir' => 'required',
+            'alamat' => 'required',
+            'jk' => 'required',
+            'datajurusans_id' => 'required',
+            'angkatan' => 'required',
+        ]);
+
+        Mahasiswa::create([
+            'nim' => $request->nim,
+            'name' => $request->name,
+            'jk' => $request->jk,
+            'tgl_lahir' => $request->tgl_lahir,
+            'alamat' => $request->alamat,
+            'datajurusans_id' => $request->datajurusans_id,
+            'angkatan' => $request->angkatan,
+        ]);
+        return redirect('/mahasiswa');
+    }
+
+    public function edit($id)
+    {
+        $jurusans = Datajurusan::all();
+        $mhs = Mahasiswa::find($id);
+        return view('mahasiswa.edit', compact('jurusans', 'mhs'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'tgl_lahir' => 'required',
+            'alamat' => 'required',
+            'jk' => 'required',
+            'datajurusans_id' => 'required',
+            'angkatan' => 'required',
+        ]);
+
+        Mahasiswa::find($id)->update([
+            'name' => $request->name,
+            'jk' => $request->jk,
+            'tgl_lahir' => $request->tgl_lahir,
+            'alamat' => $request->alamat,
+            'datajurusans_id' => $request->datajurusans_id,
+            'angkatan' => $request->angkatan,
+        ]);
+        return redirect('/mahasiswa');
+    }
+
+    public function destroy($id)
+    {
+        Mahasiswa::destroy($id);
+        return redirect('/mahasiswa');
+    }
+}
